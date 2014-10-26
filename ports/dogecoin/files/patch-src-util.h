181ac818f3697c45cedc99cb7722a7a84698861b
Charlie Root <root@testcoin.morante.com>
2014-10-26 00:55:53 -0400

    Add aricoin,dogecoin,ppcoin,legendarycoin,fireflycoin,unobtanium

---
+++ ports/dogecoin/files/patch-src-util.h
@@ -0,0 +1,10 @@
+--- src/util.h.orig	2014-08-24 11:09:34.000000000 -0400
++++ src/util.h	2014-10-17 22:23:33.000000000 -0400
+@@ -24,6 +24,7 @@
+ #include <string>
+ #include <utility>
+ #include <vector>
++#include <cstdarg>
+ 
+ #ifndef WIN32
+ #include <sys/resource.h>
