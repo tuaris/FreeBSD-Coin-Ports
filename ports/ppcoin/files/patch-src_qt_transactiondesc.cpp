6270944782c7a6c5a928bf004cdb6ba968e8fac1
Daniel Morante <daniel@morante.net>
2015-06-05 11:09:37 -0400

    Fix Peercoin Qt Wallet Build Error

---
+++ ports/ppcoin/files/patch-src_qt_transactiondesc.cpp
@@ -0,0 +1,10 @@
+--- src/qt/transactiondesc.cpp.orig	2014-07-30 19:48:08 UTC
++++ src/qt/transactiondesc.cpp
+@@ -7,6 +7,7 @@
+ #include "wallet.h"
+ #include "db.h"
+ #include "ui_interface.h"
++#include "walletdb.h"
+ 
+ #include <QString>
+ 
