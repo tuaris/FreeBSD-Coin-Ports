26c81cee3d5a860378b7930f0be70d344af99361
Daniel Morante <daniel@morante.net>
2015-07-21 14:49:39 -0400

    Add Takeicoin, Continuumcoin

---
+++ ports/takeicoin/files/patch-src_serialize.h
@@ -0,0 +1,22 @@
+--- src/serialize.h.orig	2014-03-22 00:20:39 UTC
++++ src/serialize.h
+@@ -808,19 +808,6 @@ public:
+     iterator insert(iterator it, const char& x=char()) { return vch.insert(it, x); }
+     void insert(iterator it, size_type n, const char& x) { vch.insert(it, n, x); }
+ 
+-    void insert(iterator it, const_iterator first, const_iterator last)
+-    {
+-        assert(last - first >= 0);
+-        if (it == vch.begin() + nReadPos && (unsigned int)(last - first) <= nReadPos)
+-        {
+-            // special case for inserting at the front when there's room
+-            nReadPos -= (last - first);
+-            memcpy(&vch[nReadPos], &first[0], last - first);
+-        }
+-        else
+-            vch.insert(it, first, last);
+-    }
+-
+     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
+     {
+         assert(last - first >= 0);
