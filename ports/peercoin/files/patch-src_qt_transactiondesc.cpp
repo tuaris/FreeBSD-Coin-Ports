2e7f4d6728a6678c91f099de026f14a0b68e2a47
Daniel Morante <daniel@morante.net>
2017-11-12 05:52:44 -0500

    Use correct tag for Peercoin

---
+++ ports/peercoin/files/patch-src_qt_transactiondesc.cpp
@@ -0,0 +1,10 @@
+--- src/qt/transactiondesc.cpp.orig	2017-11-07 12:11:08 UTC
++++ src/qt/transactiondesc.cpp
+@@ -7,6 +7,7 @@
+ #include "db.h"
+ #include "ui_interface.h"
+ #include "base58.h"
++#include "walletdb.h"
+ 
+ #include <string>
+ 
