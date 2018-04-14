e7b5eb3d5fb01053de3856a50d0664b4ae971d1b
Daniel Morante <daniel@morante.net>
2018-04-14 17:11:28 -0400

    Fix build for boost 1.66

--- ports/pacycoin/files/patch-src_bitcoinrpc.cpp
+++ ports/pacycoin/files/patch-src_bitcoinrpc.cpp
@@ -1,17 +1,17 @@
---- src/rpcserver.cpp.orig	2018-01-10 23:24:05 UTC
-+++ src/rpcserver.cpp
-@@ -492,8 +492,8 @@ private:
- void ServiceConnection(AcceptedConnection *conn);
+--- src/bitcoinrpc.cpp.orig	2015-07-21 18:30:38 UTC
++++ src/bitcoinrpc.cpp
+@@ -2762,8 +2762,8 @@ void ThreadRPCServer(void* parg)
+ }
  
- //! Forward declaration required for RPCListen
+ // Forward declaration required for RPCListen
 -template <typename Protocol, typename SocketAcceptorService>
 -static void RPCAcceptHandler(boost::shared_ptr< basic_socket_acceptor<Protocol, SocketAcceptorService> > acceptor,
 +template <typename Protocol>
 +static void RPCAcceptHandler(boost::shared_ptr< basic_socket_acceptor<Protocol> > acceptor,
                               ssl::context& context,
                               bool fUseSSL,
-                              boost::shared_ptr< AcceptedConnection > conn,
-@@ -502,8 +502,8 @@ static void RPCAcceptHandler(boost::shar
+                              AcceptedConnection* conn,
+@@ -2772,8 +2772,8 @@ static void RPCAcceptHandler(boost::shar
  /**
   * Sets up I/O resources to accept and handle a new connection.
   */
@@ -22,7 +22,7 @@
                     ssl::context& context,
                     const bool fUseSSL)
  {
-@@ -513,7 +513,7 @@ static void RPCListen(boost::shared_ptr<
+@@ -2783,7 +2783,7 @@ static void RPCListen(boost::shared_ptr<
      acceptor->async_accept(
              conn->sslStream.lowest_layer(),
              conn->peer,
@@ -31,7 +31,7 @@
                  acceptor,
                  boost::ref(context),
                  fUseSSL,
-@@ -525,8 +525,8 @@ static void RPCListen(boost::shared_ptr<
+@@ -2794,8 +2794,8 @@ static void RPCListen(boost::shared_ptr<
  /**
   * Accept and handle incoming connection.
   */
@@ -41,22 +41,4 @@
 +static void RPCAcceptHandler(boost::shared_ptr< basic_socket_acceptor<Protocol> > acceptor,
                               ssl::context& context,
                               const bool fUseSSL,
-                              boost::shared_ptr< AcceptedConnection > conn,
-@@ -620,7 +620,7 @@ void StartRPCThreads()
- 
-     assert(rpc_io_service == NULL);
-     rpc_io_service = new boost::asio::io_service();
--    rpc_ssl_context = new ssl::context(*rpc_io_service, ssl::context::sslv23);
-+    rpc_ssl_context = new ssl::context(ssl::context::sslv23);
- 
-     const bool fUseSSL = GetBoolArg("-rpcssl", false);
- 
-@@ -639,7 +639,7 @@ void StartRPCThreads()
-         else LogPrintf("ThreadRPCServer ERROR: missing server private key file %s\n", pathPKFile.string());
- 
-         string strCiphers = GetArg("-rpcsslciphers", "TLSv1.2+HIGH:TLSv1+HIGH:!SSLv2:!aNULL:!eNULL:!3DES:@STRENGTH");
--        SSL_CTX_set_cipher_list(rpc_ssl_context->impl(), strCiphers.c_str());
-+        SSL_CTX_set_cipher_list(rpc_ssl_context->native_handle(), strCiphers.c_str());
-     }
- 
-     std::vector<ip::tcp::endpoint> vEndpoints;
+                              AcceptedConnection* conn,
