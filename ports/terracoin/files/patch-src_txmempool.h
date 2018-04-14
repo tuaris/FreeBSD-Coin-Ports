d484bc5319dcc2aae00d43edae3846d41535800c
Daniel Morante <daniel@morante.net>
2018-04-14 04:02:45 -0400

    Remove extra patches

---
+++ ports/terracoin/files/patch-src_txmempool.h
@@ -0,0 +1,20 @@
+--- src/txmempool.h.orig	2018-01-11 20:38:04 UTC
++++ src/txmempool.h
+@@ -241,7 +241,7 @@ public:
+ class CompareTxMemPoolEntryByScore
+ {
+ public:
+-    bool operator()(const CTxMemPoolEntry& a, const CTxMemPoolEntry& b)
++    bool operator()(const CTxMemPoolEntry& a, const CTxMemPoolEntry& b) const
+     {
+         double f1 = (double)a.GetModifiedFee() * b.GetTxSize();
+         double f2 = (double)b.GetModifiedFee() * a.GetTxSize();
+@@ -255,7 +255,7 @@ public:
+ class CompareTxMemPoolEntryByEntryTime
+ {
+ public:
+-    bool operator()(const CTxMemPoolEntry& a, const CTxMemPoolEntry& b)
++    bool operator()(const CTxMemPoolEntry& a, const CTxMemPoolEntry& b) const
+     {
+         return a.GetTime() < b.GetTime();
+     }
