a2f844f91871a5f35e3dd8f9fd34462b6357b8a6
Daniel Morante <daniel@morante.net>
2021-01-19 04:24:58 -0500

    Update dash and rename to dashpay.  Add corresponding slave ports for util and daemon

---
+++ net-p2p/dashpay/files/patch-src_qt_trafficgraphwidget.cpp
@@ -0,0 +1,10 @@
+--- src/qt/trafficgraphwidget.cpp.orig	2020-11-15 21:41:25 UTC
++++ src/qt/trafficgraphwidget.cpp
+@@ -9,6 +9,7 @@
+ #include <boost/bind.hpp>
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QColor>
+ #include <QTimer>
+ 
