ee97dca23bf3869329516bc47006bbc091805aa3
Daniel Morante <daniel@morante.net>
2024-12-18 01:45:48 -0500

    Fix patch for scrypt on pepecoin

--- net-p2p/pepecoin/files/patch-src_crypto_scrypt.cpp
+++ net-p2p/pepecoin/files/patch-src_crypto_scrypt.cpp
@@ -1,10 +1,9 @@
---- src/crypto/scrypt.cpp.orig	2024-12-14 03:41:55 UTC
+--- src/crypto/scrypt.cpp.orig	2024-05-12 22:05:21 UTC
 +++ src/crypto/scrypt.cpp
-@@ -32,7 +32,7 @@
+@@ -32,6 +32,7 @@
  #include <stdlib.h>
  #include <stdint.h>
  #include <string.h>
--
 +#include <sys/endian.h>
  
  #if defined(USE_SSE2) && !defined(USE_SSE2_ALWAYS)
